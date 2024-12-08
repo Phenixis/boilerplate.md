cd998c0f202bd603026e7e8401eee74f2485e68d
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-12-08 11:36:32 +0100

    working on the sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,18 @@
 import Header from '@/components/big/header';
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <section className="flex flex-col min-h-screen">
-      <Header />
-      {children}
-    </section>
-  );
+    return (
+        <section className="flex flex-col min-h-screen">
+            <SidebarProvider>
+                <AppSidebar />
+                <div className='w-full'>
+                    <Header />
+                    <SidebarTrigger />
+                    {children}
+                </div>
+            </SidebarProvider>
+        </section>
+    );
 }
