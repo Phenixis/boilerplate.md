f2d863e4a15bfc9f7350133a6b16a32421652278
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-01 21:37:28 +0100

    feat: added the possibility to add class names to the logo

--- components/big/logo.tsx
+++ components/big/logo.tsx
@@ -1,14 +1,16 @@
-import { CircleIcon } from "lucide-react";
+import { Table2 } from "lucide-react";
 import Link from "next/link";
 
 export default function Logo({
     title = false,
+    className
 }: {
-    title?: boolean;
+    title?: boolean,
+    className?: string,
 }) {
     return (
-        <Link href="/" className="flex items-center justify-center">
-            <CircleIcon className="h-12 w-12 text-brand-primary" />
+        <Link href="/" className={`flex items-center justify-center ${className}`}>
+            <Table2 className="h-12 w-12 text-brand-primary" />
             {
                 title ? (
                     <h1 className="ml-2 text-xl font-semibold text-gray-900">
