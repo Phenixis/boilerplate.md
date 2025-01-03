815a7a431f3a99fd71738a36cddbf32936a06963
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-03 12:50:27 +0100

    feat: update Logo component to display dynamic app name and improve async functionality

--- components/big/logo.tsx
+++ components/big/logo.tsx
@@ -1,20 +1,24 @@
+'use server';
+
 import { Table2 } from "lucide-react";
 import Link from "next/link";
 
-export default function Logo({
+export default async function Logo({
     title = false,
     className
 }: {
     title?: boolean,
-    className?: string,
+    className?: string
 }) {
+    const appName = process.env.APP_NAME || '[App]';
+
     return (
         <Link href="/" className={`flex items-center justify-center ${className}`}>
             <Table2 className="h-12 w-12 text-brand-primary" />
             {
                 title ? (
                     <h1 className="ml-2 text-xl font-semibold text-gray-900">
-                        ACME
+                        {appName}
                     </h1>
                 ) : null
             }
