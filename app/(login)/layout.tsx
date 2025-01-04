9fdd21df3db6bc609c20c80d0ee13cfecef622a4
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-04 21:14:35 +0100

    feat: refactor layout to use ValuesProvider for user context and add dynamic app and company names

--- app/(login)/layout.tsx
+++ app/(login)/layout.tsx
@@ -1,4 +1,4 @@
-import { UserProvider } from "@/lib/auth";
+import { ValuesProvider } from '@/lib/auth';
 import { Manrope } from 'next/font/google';
 import { getUser } from '@/lib/db/queries';
 
@@ -10,17 +10,19 @@ export default function Layout(
     }
 ) {
     let userPromise = getUser();
+    let appName = process.env.APP_NAME || '[App]';
+    let companyName = process.env.COMPANY_NAME || 'Company';
 
     return (
         <html
             lang="en"
             className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
         >
-            <UserProvider userPromise={userPromise}>
+            <ValuesProvider userPromise={userPromise} appName={appName} companyName={companyName}>
                 <body className="min-h-screen bg-gray-50 flex flex-col justify-between">
                     {children}
                 </body>
-            </UserProvider>
+            </ValuesProvider>
         </html >
     );
 }
