5500a82137708eb9ddf6f2530751a8e6297fcd37
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-06 05:45:44 +0100

    feat: add Dark Mode toggle component with state management

---
+++ components/big/darkModeToggler.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useState } from 'react'
+import { Moon, Sun } from 'lucide-react'
+
+export default function DarkModeToggle() {
+    const [isDarkMode, setIsDarkMode] = useState(false)
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode)
+        if (isDarkMode) {
+            document.documentElement.classList.remove('dark')
+        } else {
+            document.documentElement.classList.add('dark')
+        }
+    }
+
+    return (
+        <button
+            onClick={toggleDarkMode}
+            className="-rotate-[23deg] hover:rotate-[23deg] duration-300 flex align-middle relative py-1 px-2 m-1 transition-all text-neutral-800 hover:text-neutral-500 dark:text-neutral-200 dark:hover:text-neutral-500"
+        >
+            {isDarkMode ? (
+                <Moon />
+            ) : (
+                <Sun />
+            )}
+        </button>
+    )
+}
