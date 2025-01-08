a92dae467ed21336995bce1866f4236b3d24f0e3
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-08 06:27:28 +0100

    feat: integrate Google provider and Drizzle adapter in NextAuth configuration

--- auth.ts
+++ auth.ts
@@ -1,5 +1,19 @@
 import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import { db } from "@/lib/db/drizzle"
+import { users, accounts, authenticators, sessions } from "@/lib/db/schema"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: true,
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    authenticatorsTable: authenticators,
+    sessionsTable: sessions,
+  }),
   providers: [Google],
+  pages: {
+    signIn: "sign-in",
+  },
 })
