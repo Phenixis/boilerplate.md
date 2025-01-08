b4cc0acd6783585705fe327feccf36eecb15281a
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-08 07:51:12 +0100

    feat: implement team creation logic during user sign-in and enhance session management

--- auth.ts
+++ auth.ts
@@ -1,19 +1,60 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { db } from "@/lib/db/drizzle"
-import { users, accounts, authenticators, sessions } from "@/lib/db/schema"
- 
+import { users, accounts, authenticators, sessions, NewTeam, teams, ActivityType, NewTeamMember, teamMembers } from "@/lib/db/schema"
+import authConfig from "./auth.config"
+import { getTeamForUser } from "./lib/db/queries"
+import { logActivity } from "./app/(login)/actions"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  debug: true,
   adapter: DrizzleAdapter(db, {
     usersTable: users,
     accountsTable: accounts,
     authenticatorsTable: authenticators,
     sessionsTable: sessions,
   }),
-  providers: [Google],
   pages: {
     signIn: "sign-in",
   },
+  session: { strategy: "jwt" },
+  callbacks: {
+    async signIn(params) {
+      if (!params?.user.id) {
+        return false;
+      }
+
+      const userWithTeam = await getTeamForUser(params?.user.id);
+      
+      if (!userWithTeam) {
+        const newTeam: NewTeam = {
+          name: `${params?.user.email}'s Team`,
+        };
+
+        const [createdTeam] = await db.insert(teams).values(newTeam).returning();
+
+        if (!createdTeam) {
+          return false;
+        }
+
+        const teamId = createdTeam.id;
+        const userRole = 'owner';
+
+        await logActivity(teamId, params?.user.id, ActivityType.CREATE_TEAM);
+
+        const newTeamMember: NewTeamMember = {
+          userId: params?.user.id,
+          teamId: teamId,
+          role: userRole,
+        };
+
+        await Promise.all([
+          db.insert(teamMembers).values(newTeamMember),
+          logActivity(teamId, params?.user.id, ActivityType.SIGN_UP),
+        ]);
+      }
+
+      return true
+    }
+  },
+  ...authConfig,
 })
