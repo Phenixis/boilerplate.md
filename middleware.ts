30d8e4f4a46378b30b97098dfea26805a3d27674
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-10 05:48:59 +0100

    feat: enhance middleware to redirect users based on authentication status

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from 'next/server';
 import { getSession } from '@/lib/auth/session';
 import NextAuth from 'next-auth';
 import authConfig from './auth.config';
+import { getUser } from './lib/db/queries';
 
 const protectedRoutes = '/dashboard';
 const unaccessibleWhenConnected = ['/sign-in', '/sign-up'];
@@ -18,7 +19,9 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
 
-  if (session && unaccessibleWhenConnected.includes(pathname)) {
+  const user = await getUser();
+
+  if (user && unaccessibleWhenConnected.includes(pathname)) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 }
