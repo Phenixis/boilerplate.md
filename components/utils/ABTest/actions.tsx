4b7131da10e3a0faa312e58885ccdbb949dc2fe5
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-15 06:14:06 +0100

    feat: enhance Leaves function to include user IP and test ID for better tracking and create stillIn function to update the last time the user has been in the page

--- components/utils/ABTest/actions.tsx
+++ components/utils/ABTest/actions.tsx
@@ -1,7 +1,13 @@
 "use server";
 
-export async function Leaves(startTime: number) {
+export async function Leaves(userIp: string, idTest: string, startTime: number) {
     const endTime = performance.now()
     const queryTimeMs = (endTime - startTime).toFixed(2);
-    console.log(`User is leaving the page after ${queryTimeMs} ms`);
+    console.log(`User ${userIp} is leaving the page with the test ${idTest} after ${queryTimeMs} ms`);
+}
+
+export async function stillIn(userIp: string, idTest: string, startTime: number) {
+    const endTime = performance.now()
+    const queryTimeMs = (endTime - startTime).toFixed(2);
+    console.log(`User ${userIp} is still in the page with the test ${idTest} after ${queryTimeMs} ms`);
 }
