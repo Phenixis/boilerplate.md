efb4eb97dd56246daab8e558ad34fd4b75a03aa5
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-15 06:14:13 +0100

    feat: refactor ABTestClient to use stillIn function for tracking user visibility and implement variant selection

--- components/utils/ABTest/ABTestClient.tsx
+++ components/utils/ABTest/ABTestClient.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Leaves } from './actions'
-import { useEffect } from 'react';
+import { stillIn } from './actions'
+import { useEffect, useState } from 'react';
 
 export default function ABTestClient({
     A,
@@ -16,26 +16,21 @@ export default function ABTestClient({
     userIp: string;
     startTime: number;
 }) {
-    // TODO : Maybe change to visiblitychange event, to track when the user leaves the page or not, and calculate the time spent on the page
-    // Visiblitychange accepts async functions / beforeunload does not
-    useEffect(() => { // Once the window has loaded, we add an event listener to the window object to handle the beforeunload event
-        const handleBeforeUnload = (event: any) => { // When the user leaves, we call the Leaves function to calculate the time spent on the page
-            event.preventDefault();
-            Leaves(startTime)
-            event.returnValue = '';
-        }
+    const [variant, setVariant] = useState('A');
 
-        window.addEventListener('beforeunload', handleBeforeUnload)
+    useEffect(() => {
+        setVariant(Math.random() < 0.5 ? 'A' : 'B');
 
-        return () => {
-            window.removeEventListener('beforeunload', handleBeforeUnload)
-        };
-    }, [startTime])
+        setInterval(() => {
+            if (document.visibilityState === 'visible') {
+                stillIn(userIp, idTest, startTime);
+            }
+        }, 1000);
+    }, []);
 
     return (
-        <div>
-            {A}
-            {B}
-        </div>
+        <>
+            {variant === 'A' ? A : B}
+        </>
     )
 }
