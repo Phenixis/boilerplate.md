0c277fe861a279655804cc605120b9f9ce834847
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-19 22:29:57 +0100

    feat: update sendFeedback function to accept state and data parameters

--- components/Feedback/actions.ts
+++ components/Feedback/actions.ts
@@ -1,5 +1,14 @@
+"use server";
 
+import { desc, and, eq, isNull, or, isNotNull, count } from 'drizzle-orm';
+import { db } from '@/lib/db/drizzle';
+import {  } from '@/lib/db/schema';
+import { ActionState } from '@/lib/auth/middleware';
 
-export async  function sendFeedback() {
+export async  function sendFeedback(state: ActionState, data: FormData) {
+    // TODO: define the schema for the feedback table
+    // TODO: insert the feedback into the feedback table
+    // TODO: define it as a ticket : these should be accessible from the settings with a ticketing system
 
+    return {success: 'Feedback sent successfully'};
 }
