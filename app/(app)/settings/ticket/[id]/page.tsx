6fda14b8dd170ef3d3856027dd5be670cb63dc0d
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-20 10:06:10 +0100

    feat: create Ticket page with comments display and user validation

---
+++ app/(app)/settings/ticket/[id]/page.tsx
@@ -0,0 +1,67 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { getTicket, getComments } from "@/components/feedback/actions";
+import { getUser } from "@/lib/db/queries";
+import { redirect } from "next/navigation";
+import { Badge } from '@/components/ui/badge';
+import { formatDistanceToNow } from 'date-fns';
+
+export default async function Page({
+    params
+}: {
+    params: Promise<{ id: string }>
+}) {
+    const id = (await params).id;
+    const ticket = await getTicket(parseInt(id));
+    const creationTime = formatDistanceToNow(new Date(ticket.createdAt), { addSuffix: true });
+    const user = await getUser();
+    const comments = await getComments(parseInt(id));
+
+    if (!user) {
+        redirect('/login');
+    }
+
+    if (ticket.openedBy !== user.id && ticket.openerEmail !== user.email) {
+        redirect('/settings/ticket');
+    }
+
+
+    return (
+        <section className="flex-1 p-4 lg:p-8">
+            <div className="flex justify-between items-center mb-6">
+                <h1 className="text-lg lg:text-2xl font-medium text-gray-900 dark:text-gray-100">
+                    <span className="text-gray-300 dark:text-gray-700">#{ticket.id}</span> {ticket.title}
+                </h1>
+                <Badge variant="outline">{ticket.status}</Badge>
+            </div>
+            <Card>
+                <CardHeader>
+                    <CardTitle>{user.name} wrote {creationTime} :</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <p>
+                        {ticket.description}
+                    </p>
+                </CardContent>
+            </Card>
+            {
+                comments.map((comment) => (
+                    <>
+                        <hr className="ml-4 my-6 w-12 rotate-90" />
+                        <Card key={comment.id} className="mt-4">
+                            <CardHeader>
+                                <CardTitle>{
+                                getUser(comment.userId).then((user) => user?.name)
+                                } wrote {formatDistanceToNow(new Date(comment.createdAt), { addSuffix: true })} :</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p>
+                                    {comment.comment}
+                                </p>
+                            </CardContent>
+                        </Card>
+                    </>
+                ))
+            }
+        </section>
+    )
+}
