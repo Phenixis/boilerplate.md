96348185a68f2d7ac89ded59f656110e3c8da291
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-20 05:47:52 +0100

    feat: comment out ABTest component in HomePage for testing purposes

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -7,7 +7,7 @@ import ABTest from '@/components/utils/ABTest/ABTest';
 export default function HomePage() {
     return (
         <main>
-            <ABTest A={<h1>Test A</h1>} B={<h1>Test B</h1>} testName="homepage_heading" description="A test to determine which headline make the user stay longer on the page" location="/" />
+            {/* <ABTest A={<h1>Test A</h1>} B={<h1>Test B</h1>} testName="homepage_heading" description="A test to determine which headline make the user stay longer on the page" location="/" /> */}
             <section className="py-16">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="lg:grid lg:grid-cols-12 lg:gap-8">
