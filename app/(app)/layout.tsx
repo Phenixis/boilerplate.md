c53a8fd11314ee36d8e9da3dee370fd5b0208e3e
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-20 10:15:43 +0100

    feat: update layout component to support full-width Header and Footer

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -8,9 +8,9 @@ export default function Layout({
 }) {
     return (
         <main className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col justify-between">
-            <Header />
+            <Header fullWidth />
             {children}
-            <Footer />
+            <Footer fullWidth />
         </main>
     )
 }
