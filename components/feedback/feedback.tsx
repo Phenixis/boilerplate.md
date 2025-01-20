b771da52fcf5f0f5e7fc69265f205350700e7499
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-20 08:44:38 +0100

    feat: add Textarea component and update Feedback form to use it

--- components/feedback/feedback.tsx
+++ components/feedback/feedback.tsx
@@ -18,6 +18,7 @@ import { useEffect, useActionState, useState } from "react";
 import { ActionState } from "@/lib/auth/middleware";
 import { Loader2 } from "lucide-react";
 import { useToast } from "@/hooks/use-toast"
+import { Textarea } from "../ui/textarea";
 
 export default function Feedback() {
     const user = useValues().user;
@@ -58,26 +59,24 @@ export default function Feedback() {
 
     return (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
-            <DialogTrigger>
-                <Button className="fixed bottom-0 right-0 rounded-none rounded-tl-md duration-300">
-                    {title}
-                </Button>
+            <DialogTrigger className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 h-9 px-4 py-2 bg-primary text-primary-foreground shadow hover:bg-primary/90 fixed bottom-0 right-0 rounded-none rounded-tl-md duration-300">
+                {title}
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Send feedback</DialogTitle>
                     <DialogDescription>
-                        Please let me know how I can improve this app. You can also contact me on <a href="https://twitter.com/maxime_duhamel_" target="_blank" className="underline hover:text-gray-900">X</a>.
+                        Please let me know how I can improve this app. You can also contact me on <a href="https://twitter.com/maxime_duhamel_" target="_blank" className="underline hover:text-gray-900">𝕏</a>.
                     </DialogDescription>
                 </DialogHeader>
                 <form action={feedbackAction} className="gap-4 flex flex-col">
                     {
                         user ?
-                        <input className="hidden" name="userId" id="userId" value={user?.id} readOnly />
-                        : <div>
-                            <Label>Email</Label>
-                            <Input type="email" name="userEmail" id="userEmail" required />
-                        </div>
+                            <input className="hidden" name="userId" id="userId" value={user?.id} readOnly />
+                            : <div>
+                                <Label>Email</Label>
+                                <Input type="email" name="userEmail" id="userEmail" required />
+                            </div>
                     }
                     <div>
                         <Label>Title</Label>
@@ -85,7 +84,7 @@ export default function Feedback() {
                     </div>
                     <div>
                         <Label>Description</Label>
-                        <Input type="textarea" name="description" id="description" required />
+                        <Textarea name="description" id="description" required />
                     </div>
                     <div className="flex justify-end gap-4">
                         <DialogClose asChild>
