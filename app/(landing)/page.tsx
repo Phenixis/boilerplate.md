9434f661c259c19e9988d57328f7b7b234b4915c
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-21 06:13:09 +0100

    feat: update landing page content and layout for enhanced user focus and engagement

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -7,27 +7,25 @@ export default function HomePage() {
     return (
         <main>
             {/* <ABTest A={<h1>Test A</h1>} B={<h1>Test B</h1>} testName="homepage_heading" description="A test to determine which headline make the user stay longer on the page" location="/" /> */}
-            <section className="py-16">
+            <section className="py-32">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="lg:grid lg:grid-cols-12 lg:gap-8">
-                        <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+                    <div className="">
+                        <div className="sm:text-center md:max-w-2xl md:mx-auto">
                             <h1 className="text-4xl font-bold text-gray-900 dark:text-slate-100 tracking-tight sm:text-5xl md:text-6xl">
-                                Focus on
-                                <strong className="block text-brand-primary">the value proposition</strong>
+                                Forget the fluff, 
+                                <strong className="block text-brand-primary">Focus on your genius</strong>
                             </h1>
-                            <ol className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                                <li>Describe the actual state</li>
-                                <li>Describe what they could get with the product</li>
-                                <li>Describe what they could lose if they don't use the product</li>
-                            </ol>
-                            <div className="mt-8 space-y-2 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
+                            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                                Start your brilliant idea with all the fluff already done and build your next startup in days.
+                            </p>
+                            <div className="mt-8 space-y-2 sm:max-w-lg sm:mx-auto sm:text-center">
                                 <CTA />
                                 <SocialProof />
                             </div>
                         </div>
-                        <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+                        {/* <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
                             <Terminal />
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </section>
