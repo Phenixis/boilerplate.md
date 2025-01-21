6e536fa2f983f6e6f61ad59f181e28b12d2fae85
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-21 06:13:05 +0100

    feat: enhance CTA button styling and text for improved user engagement

--- components/big/cta.tsx
+++ components/big/cta.tsx
@@ -3,13 +3,16 @@ import { Button } from "../ui/button";
 
 export default function CTA() {
     return (
-            <a
-                href="/sign-up"
-            >
-                <Button className="bg-white hover:bg-gray-100 text-black border border-gray-200 rounded-full text-lg p-6 inline-flex items-center justify-center">
-                    Call To Action With Urgency
-                    <ArrowRight className="ml-2 h-5 w-5" />
-                </Button>
-            </a>
+        <a href="/sign-up">
+            <Button className="bg-white hover:bg-gray-100 text-black border border-brand-primary shadow shadow-brand-primary/30 rounded-full text-lg p-6 inline-flex items-center justify-center group/CTA gap-1">
+                Start to
+                <span className="relative overflow-hidden">
+                    <span className="relative block before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-full before:bg-black before:transform before:scale-x-0 before:origin-left before:transition-transform before:duration-300 group-hover/CTA:before:scale-x-100">
+                        build now
+                    </span>
+                </span>
+                <ArrowRight className="ml-2 h-5 w-5 -translate-x-1 group-hover/CTA:translate-x-1 duration-300" />
+            </Button>
+        </a>
     );
 }
