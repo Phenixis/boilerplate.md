395b60490cbe49540161ef7290a1714aff17c310
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-21 13:58:18 +0100

    feat: add safelist for dynamic row and column span classes in Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,11 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+	{
+		pattern: /((row)|(col))-span-\d/gm
+	}
+  ],
   theme: {
   	extend: {
   		borderRadius: {
