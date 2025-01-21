c8af89dca4e2d369ccff5d9d9873619611d891c2
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-21 05:34:26 +0100

    feat: refactor DashboardLayout to integrate collapsible sidebar navigation and improve layout structure

--- app/(app)/settings/layout.tsx
+++ app/(app)/settings/layout.tsx
@@ -4,38 +4,30 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { redirect, usePathname } from 'next/navigation';
 import { Button } from '@/components/ui/button';
-import { Users, Settings, Shield, Activity, Ticket, Home, LogOut } from 'lucide-react';
-import { signOut } from '@/app/(login)/actions';
-import { useRouter } from 'next/navigation';
+import { Users, Settings, Shield, Activity, Ticket, Slash, Menu, ChevronDown, SettingsIcon, Home, LayoutDashboard } from 'lucide-react';
 import { useValues } from '@/lib/auth';
 import Feedback from '@/components/feedback/feedback';
 import DarkModeToggle from '@/components/big/darkModeToggler';
 import {
-    SidebarProvider,
-    SidebarTrigger,
     Sidebar,
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
     SidebarHeader,
+    useSidebar,
 } from "@/components/ui/sidebar"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import {
     ChevronsLeftRightIcon
 } from '@/components/ui/chevrons-left-right';
 import {
     ChevronsRightLeftIcon
 } from '@/components/ui/chevrons-right-left';
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage
-} from '@/components/ui/avatar';
+import Logo from '@/components/big/logo';
+import UserAvatar from '@/components/big/userAvatar';
+import Breadcrumb from '@/components/big/breadcrumb';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
 
 export default function DashboardLayout({
     children,
@@ -43,21 +35,20 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }) {
     const { user, setUser } = useValues();
+
     if (!user) {
         redirect('/login');
     }
+
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const router = useRouter();
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const pathname = usePathname();
+    const { open, toggleSidebar } = useSidebar();
 
-    async function handleSignOut() {
-        setUser(null);
-        await signOut();
-        router.push('/');
-    }
+    const dashboardNavItems = [
+        { href: '/dashboard', icon: LayoutDashboard, label: 'Dashboard' },
+    ]
 
-    const navItems = [
+    const settingsNavItems = [
         { href: '/settings', icon: Settings, label: 'General' },
         { href: '/settings/team', icon: Users, label: 'Team' },
         { href: '/settings/activity', icon: Activity, label: 'Activity' },
@@ -65,82 +56,86 @@ export default function DashboardLayout({
         { href: '/settings/ticket', icon: Ticket, label: 'Ticket' },
     ];
 
+    const navItems = {
+        'Dashboard': dashboardNavItems,
+        'Settings': settingsNavItems,
+    }
+
     return (
-        <SidebarProvider open={isSidebarOpen} onOpenChange={setIsSidebarOpen}>
+        <>
             <Sidebar collapsible='icon'>
-                <SidebarContent>
-                    <nav className="h-full overflow-y-auto p-2">
-                        {navItems.map((item) => (
-                            <Link key={item.href} href={item.href} passHref>
-                                <Button
-                                    variant={pathname === item.href ? 'secondary' : 'ghost'}
-                                    className={`p-2 mt-1 w-full justify-start ${pathname === item.href ? 'bg-gray-100' : ''
-                                        }`}
-                                    onClick={() => setIsSidebarOpen(false)}
-                                >
-                                    <item.icon className="size-4" />
-                                    {isSidebarOpen && item.label}
-                                </Button>
-                            </Link>
-                        ))}
-                    </nav>
+                <SidebarContent className="gap-0">
+                    <SidebarHeader>
+                        <Logo title={open} className="py-4" />
+                    </SidebarHeader>
+                    {
+                        Object.entries(navItems).map(([key, item]) => {
+                            return (
+                                <Collapsible defaultOpen key={key} className="group/collapsible">
+                                    <SidebarGroup>
+                                        <SidebarGroupLabel asChild>
+                                            <CollapsibleTrigger>
+                                                {key}
+                                                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                                            </CollapsibleTrigger>
+                                        </SidebarGroupLabel>
+                                        <CollapsibleContent>
+                                            <nav className="h-full overflow-y-auto p-2">
+                                                {item.map((subItem) => (
+                                                    <Link key={subItem.href} href={subItem.href} passHref>
+                                                        <Button
+                                                            variant={pathname === subItem.href ? 'secondary' : 'ghost'}
+                                                            className={`p-2 mt-1 w-full justify-start ${pathname === subItem.href ? 'bg-gray-100' : ''
+                                                                }`}
+                                                            onClick={toggleSidebar}
+                                                        >
+                                                            <subItem.icon className="size-4" />
+                                                            {open && subItem.label}
+                                                        </Button>
+                                                    </Link>
+                                                ))}
+                                            </nav>
+                                        </CollapsibleContent>
+                                    </SidebarGroup>
+                                </Collapsible>
+                            )
+                        })
+                    }
                 </SidebarContent>
-                <SidebarFooter className={`${isSidebarOpen && 'flex-row'} w-full justify-between items-center`}>
+                <SidebarFooter className={`${open && 'flex-row'} w-full justify-between items-center hidden md:flex`}>
                     <DarkModeToggle />
                     <Feedback />
-                    <DropdownMenu open={isMenuOpen} onOpenChange={setIsMenuOpen}>
-                        <DropdownMenuTrigger asChild>
-                            <div className={`flex space-x-2 items-center p-2 rounded-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800`}>
-                                <Avatar className="cursor-pointer size-8">
-                                    <AvatarImage src={user.image || ''} alt={user.name || ''} />
-                                    <AvatarFallback>
-                                        {
-                                            (user.name ? user.name : user.email)
-                                                .split(' ')
-                                                .map((n) => n[0])
-                                                .join('')
-                                        }
-                                    </AvatarFallback>
-                                </Avatar>
-                            </div>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end" className="flex flex-col gap-1">
-                            <DropdownMenuItem className="cursor-pointer">
-                                <Link href="/dashboard" className="flex w-full items-center">
-                                    <Home className="mr-2 h-4 w-4" />
-                                    <span>Dashboard</span>
-                                </Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem className="cursor-pointer">
-                                <Link href="/settings" className="flex w-full items-center">
-                                    <Settings className="mr-2 h-4 w-4" />
-                                    <span>Settings</span>
-                                </Link>
-                            </DropdownMenuItem>
-                            <form action={handleSignOut} className="w-full">
-                                <button type="submit" className="flex w-full">
-                                    <DropdownMenuItem className="w-full flex-1 cursor-pointer">
-                                        <LogOut className="mr-2 h-4 w-4" />
-                                        <span>Sign out</span>
-                                    </DropdownMenuItem>
-                                </button>
-                            </form>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                    <UserAvatar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} user={user} setUser={setUser} />
                     <Button
                         variant={'ghost'}
                         className={`p-2 justify-start`}
-                        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+                        onClick={toggleSidebar}
                     >
-                        {isSidebarOpen ?
+                        {open ?
                             <ChevronsRightLeftIcon />
                             : <ChevronsLeftRightIcon />}
                     </Button>
                 </SidebarFooter>
             </Sidebar>
-            <main className="flex-1 overflow-y-auto p-0 lg:p-4">
+            <main className="flex-1 overflow-y-auto p-0 md:p-4">
+                <header className="flex justify-between items-center w-full px-4 py-1 md:hidden">
+                    <Button
+                        variant={'ghost'}
+                        className={`p-2`}
+                        onClick={toggleSidebar}
+                    >
+                        <Menu />
+                    </Button>
+                    <Logo title />
+                    <div className="flex items-center">
+                        <DarkModeToggle />
+                        <Feedback />
+                        <UserAvatar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} user={user} setUser={setUser} />
+                    </div>
+                </header>
+                <Breadcrumb pathname={pathname} />
                 {children}
             </main>
-        </SidebarProvider>
+        </>
     );
 }
