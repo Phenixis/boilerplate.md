eeecc02879fb5ad6acf5e1ad8fe0deeeae273f59
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-22 11:26:36 +0100

    remove hydration error

--- components/big/breadcrumb.tsx
+++ components/big/breadcrumb.tsx
@@ -5,7 +5,7 @@ import { Breadcrumb as Bc, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, Bread
 
 export default function Breadcrumb() {
     const pathname = usePathname();
-    
+
     return (
         <Bc className="px-4 lg:px-8">
             <BreadcrumbList>
@@ -16,18 +16,20 @@ export default function Breadcrumb() {
                     pathname.split('/').map((path, index) => {
                         if (path === '') return null;
                         return (
-                            <BreadcrumbItem key={index}>
+                            <>
                                 <BreadcrumbSeparator />
-                                {
-                                    index === pathname.split('/').length - 1 ? (
-                                        <BreadcrumbPage>{path[0].toUpperCase() + path.slice(1)}</BreadcrumbPage>
-                                    ) : (
-                                        <BreadcrumbLink href={
-                                            pathname.split('/').slice(0, index + 1).join('/')
-                                        }>{path[0].toUpperCase() + path.slice(1)}</BreadcrumbLink>
-                                    )
-                                }
-                            </BreadcrumbItem>
+                                <BreadcrumbItem key={index}>
+                                    {
+                                        index === pathname.split('/').length - 1 ? (
+                                            <BreadcrumbPage>{path[0].toUpperCase() + path.slice(1)}</BreadcrumbPage>
+                                        ) : (
+                                            <BreadcrumbLink href={
+                                                pathname.split('/').slice(0, index + 1).join('/')
+                                            }>{path[0].toUpperCase() + path.slice(1)}</BreadcrumbLink>
+                                        )
+                                    }
+                                </BreadcrumbItem>
+                            </>
                         )
                     })
                 }
