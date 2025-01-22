187b5e19914c6a9f7796e7206c6108f907c8396b
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-22 15:48:11 +0100

    feat: enable dynamic rendering for NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import Logo from '@/components/big/logo';
 import { getUser } from '@/lib/db/queries';
 
+export const dynamic = 'force-dynamic'
+
 export default async function NotFound() {
   const user = await getUser();
 
