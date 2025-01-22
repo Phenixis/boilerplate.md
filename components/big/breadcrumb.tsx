2a4c0d544d72f44f3d32c8fc9cd21fa35a0e1f8d
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-22 12:06:16 +0100

    fixed a hydration error again

--- components/big/breadcrumb.tsx
+++ components/big/breadcrumb.tsx
@@ -6,30 +6,33 @@ import { Breadcrumb as Bc, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, Bread
 export default function Breadcrumb() {
     const pathname = usePathname();
 
+    console.log(pathname.split('/'));
+
     return (
         <Bc className="px-4 lg:px-8">
             <BreadcrumbList>
-                <BreadcrumbItem>
-                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                </BreadcrumbItem>
                 {
                     pathname.split('/').map((path, index) => {
-                        if (path === '') return null;
-                        return (
-                            <>
-                                <BreadcrumbSeparator />
-                                <BreadcrumbItem key={index}>
-                                    {
-                                        index === pathname.split('/').length - 1 ? (
-                                            <BreadcrumbPage>{path[0].toUpperCase() + path.slice(1)}</BreadcrumbPage>
-                                        ) : (
-                                            <BreadcrumbLink href={
-                                                pathname.split('/').slice(0, index + 1).join('/')
-                                            }>{path[0].toUpperCase() + path.slice(1)}</BreadcrumbLink>
-                                        )
-                                    }
+                        if (path === '') {
+                            return (
+                                <BreadcrumbItem>
+                                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
                                 </BreadcrumbItem>
-                            </>
+                            );
+                        }
+
+                        return (
+                            <BreadcrumbItem key={index} className="pl-2 border-l">
+                                {
+                                    index === pathname.split('/').length - 1 ? (
+                                        <BreadcrumbPage>{path[0].toUpperCase() + path.slice(1)}</BreadcrumbPage>
+                                    ) : (
+                                        <BreadcrumbLink href={
+                                            pathname.split('/').slice(0, index + 1).join('/')
+                                        }>{path[0].toUpperCase() + path.slice(1)}</BreadcrumbLink>
+                                    )
+                                }
+                            </BreadcrumbItem>
                         )
                     })
                 }
