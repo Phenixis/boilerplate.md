cf3f0b7c70ec93d5b5d8ccad081296fefbf2e30a
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-22 06:17:49 +0100

    style: format JSX for improved readability in admin page component

--- app/(app)/settings/admin/page.tsx
+++ app/(app)/settings/admin/page.tsx
@@ -1,5 +1,6 @@
 export default function Page() {
-    return (<section className="flex-1 p-4 lg:p-8">
+    return (
+    <section className="flex-1 p-4 lg:p-8">
         <h1 className="text-lg lg:text-2xl font-medium text-gray-900 dark:text-gray-100 mb-6">
             Admin
         </h1>
