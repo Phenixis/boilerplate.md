f7620cb8fa4d948a56c3cea5fcf39a3a63a3daf7
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-24 06:31:19 +0100

    feat: enhance product addition by including price and currency handling

--- app/(app)/settings/admin/pricing/actions.ts
+++ app/(app)/settings/admin/pricing/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { stripe } from "@/lib/payments/stripe";
+import Stripe from "stripe";
 
 type ActionState = {
     error?: string;
@@ -8,9 +9,36 @@ type ActionState = {
     [key: string]: any; // This allows for additional properties
 };
 
-export async function addProduct(previousState: ActionState, formData: FormData) {
-    const { name, description } = Object.fromEntries(formData.entries());
+export async function addProductAndDefaultPrice(previousState: ActionState, formData: FormData) {
+    const { name, description, price, currency } = formData.entries().reduce((acc, [key, value]) => {
+        acc[key] = value as string;
+        return acc;
+    }, {} as Record<string, string>);
 
+    if (!name || !description || !price || !currency) {
+        return {
+            error: 'Please fill out all fields',
+        };
+    }
+
+    const productResponse = await addProduct(name, description);
+
+    if (!productResponse.productId) {
+        return productResponse;
+    }
+
+    const priceResponse = await addPrice(productResponse.productId, parseInt(price), currency, formData.get("interval") as Stripe.PriceCreateParams.Recurring.Interval, parseInt(formData.get("trial_period_days") as string));
+
+    if (priceResponse.error) {
+        return priceResponse;
+    }
+
+    return {
+        success: 'Product and price added successfully',
+    };
+}
+
+export async function addProduct(name: string, description: string) {
     try {
         const product = await stripe.products.create({
             name: name as string,
@@ -27,5 +55,32 @@ export async function addProduct(previousState: ActionState, formData: FormData)
             error: error.message,
         };
     }
+}
 
+export async function addPrice(productId: string, price: number, currency: string, interval?: Stripe.PriceCreateParams.Recurring.Interval, trial_period_days?: number) {
+
+    const priceData = {
+        product: productId,
+        unit_amount: price,
+        currency: currency,
+    } as Stripe.PriceCreateParams;
+
+    if (interval) {
+        priceData.recurring = {
+            interval: interval,
+            trial_period_days: trial_period_days,
+        };
+    }
+
+    try {
+        await stripe.prices.create(priceData);
+
+        return {
+            success: 'Price added successfully',
+        };
+    } catch (error: any) {
+        return {
+            error: error.message,
+        };
+    }
 }
