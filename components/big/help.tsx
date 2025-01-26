306094a6a708e5dd2e062b5814985f5c171555cb
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-26 09:30:37 +0100

    feat: add Help component for displaying contextual help with tooltip

---
+++ components/big/help.tsx
@@ -0,0 +1,30 @@
+import { CircleHelp } from "lucide-react";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+
+export default function Help({
+    text
+} : {
+    text: string;
+}) {
+    return (
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger>
+                    <CircleHelp className="size-4 text-gray-400" />
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>
+                        {text}
+                    </p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+
+    );
+}
