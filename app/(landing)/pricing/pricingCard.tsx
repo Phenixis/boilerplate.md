cbad299b331f468ef0255bb863c09021e64901c7
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-26 09:30:34 +0100

    feat: add PricingCard component for displaying pricing options with trial information

---
+++ app/(landing)/pricing/pricingCard.tsx
@@ -0,0 +1,57 @@
+import { SubmitButton } from './submit-button';
+import { checkoutAction } from '@/lib/payments/actions';
+import { Check } from 'lucide-react';
+import { Card, CardContent, CardHeader, CardFooter, CardTitle } from '@/components/ui/card';
+
+export default function PricingCard({
+    name,
+    price,
+    interval,
+    trialDays,
+    features,
+    priceId,
+}: {
+    name: string;
+    price: number;
+    interval: string;
+    trialDays: number;
+    features: string[];
+    priceId?: string;
+}) {
+    return (
+        <Card className="flex flex-col justify-between">
+            <CardHeader>
+                <CardTitle>{name}</CardTitle>
+                <p className="text-4xl font-medium text-gray-900 dark:text-gray-100 mb-6">
+                    ${price / 100}{' '}
+                    <span className="text-xl font-normal text-gray-600">
+                        per user / {interval}
+                    </span>
+                </p>
+            </CardHeader>
+            <CardContent>
+                {
+                    trialDays > 0 && (
+                        <p className="text-sm text-gray-600 mb-4">
+                            with {trialDays} day free trial
+                        </p>
+                    )
+                }
+                <ul className="space-y-4 mb-8">
+                    {features.map((feature, index) => (
+                        <li key={index} className="flex items-start">
+                            <Check className="h-5 w-5 text-primary mr-2 mt-0.5 flex-shrink-0" />
+                            <span className="text-gray-700">{feature}</span>
+                        </li>
+                    ))}
+                </ul>
+            </CardContent>
+            <CardFooter>
+                <form action={checkoutAction}>
+                    <input type="hidden" name="priceId" value={priceId} />
+                    <SubmitButton />
+                </form>
+            </CardFooter>
+        </Card>
+    )
+}
