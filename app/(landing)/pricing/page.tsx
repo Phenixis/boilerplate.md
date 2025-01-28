c8ad484d318b3aea60b41da49beacd67be923fdc
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-28 06:32:56 +0100

    feat: adjust grid layout for pricing page to improve responsiveness and accommodate larger screens

--- app/(landing)/pricing/page.tsx
+++ app/(landing)/pricing/page.tsx
@@ -18,8 +18,8 @@ export default async function PricingPage() {
   return (
     <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className={`grid ${
-        products.length > 2 ? 'grid-cols-3' : products.length > 1 ? 'grid-cols-2' : 'grid-cols-1'
-      } gap-8 max-w-xl mx-auto`}>
+        products.length > 2 ? 'md:grid-cols-3' : products.length > 1 ? 'md:grid-cols-2' : 'md:grid-cols-1'
+      } gap-8 max-w-4xl mx-auto grid-cols-1`}>
         {prices.sort((a, b) => (a.unitAmount || 0) - (b.unitAmount || 0)).map((price) => {
           const product = products.find(
             (product) => product.id === price.productId
