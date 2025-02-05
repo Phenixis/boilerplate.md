909d8921cae7666fb39136bc0a97c856592ad5b3
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-02-05 14:16:56 +0100

    moved sidebar into (app) layout instead of (settings)

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,4 +1,6 @@
 import { SidebarProvider } from '@/components/ui/sidebar';
+import Breadcrumb from '@/components/big/breadcrumb';
+import { Sidebar, MobileSidebar } from '@/components/big/sidebar';
 
 export default function Layout({
     children,
@@ -8,7 +10,12 @@ export default function Layout({
     return (
         <main className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col justify-between">
             <SidebarProvider>
-                {children}
+                <Sidebar />
+                <main className="flex-1 overflow-y-auto p-0 md:p-4">
+                    <MobileSidebar />
+                    <Breadcrumb />
+                    {children}
+                </main>
             </SidebarProvider>
         </main>
     )
