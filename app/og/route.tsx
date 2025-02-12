ab66ee9dbeac651ae30ed4fc77c5eda3a9bf89cd
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-02-12 16:37:57 +0100

    feat: enhance Open Graph image layout with updated styling and additional text

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,27 +1,28 @@
 import { ImageResponse } from 'next/og'
 
 export function GET(request: Request) {
-  let url = new URL(request.url)
-  let title = url.searchParams.get('title') || 'Next.js Portfolio Starter'
-  let appName = process.env.APP_NAME || "App Name"
-  let companyName = process.env.COMPANY_NAME || "App Name"
+    let url = new URL(request.url)
+    let title = url.searchParams.get('title') || 'Next.js Portfolio Starter'
+    let appName = process.env.APP_NAME || "App Name"
+    let companyName = process.env.COMPANY_NAME || "App Name"
 
-  return new ImageResponse(
-    (
-      <div tw="flex flex-col w-full h-full items-start justify-start bg-white p-4">
-        <div tw="flex">
-          <h1 tw="text-4xl font-bold text-primary">
-            {appName}
-          </h1>
-        </div>
-        <h2>
-          Brought to you by {companyName}
-        </h2>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  )
+    return new ImageResponse(
+        (
+            <div tw="flex flex-col w-full h-full items-center justify-center bg-blue-300 p-4">
+                <h1 tw="text-7xl">
+                    {appName}
+                </h1>
+                <h2 tw="text-lg">
+                    {companyName}
+                </h2>
+                <p tw="text-3xl">
+                    Start your brilliant idea, with all the fluff already done
+                </p>
+            </div>
+        ),
+        {
+            width: 1200,
+            height: 630,
+        }
+    )
 }
